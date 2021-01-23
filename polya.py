0257a026c1d3a29e7ef4078ddc9c72c33a9f6d98
GregorSamsa42 <gk445@cam.ac.uk>
2021-01-23 18:07:24 +0000

    Create polya.py

---
+++ polya.py
@@ -0,0 +1,14 @@
+import random
+
+def append(x,y):
+	r = random.randint(1, x+y)
+	if r > x:
+		return (x,y+1)
+	if r <= x:
+		return (x+1,y)
+    
+def polya(n):
+	(p,q) = (1,1);
+	for x in range(1, n):
+		(p,q) = append(p,q);
+	print(ratio(p,q));
